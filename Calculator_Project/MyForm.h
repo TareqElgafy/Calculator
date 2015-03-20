3a6081ff2d3bf040269e33950a2eb6d7c5bd7f4a
Tarek_Elgafy <tarekkzero@gmail.com>
2015-03-20 21:09:37 +0200

    Clear button

--- Calculator_Project/MyForm.h
+++ Calculator_Project/MyForm.h
@@ -37,6 +37,7 @@ namespace Calculator_Project {
 	private: System::Windows::Forms::Button^  button1;
 	protected:
 	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button2;
 
 	private:
 		/// <summary>
@@ -53,13 +54,15 @@ namespace Calculator_Project {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(44, 165);
+			this->button1->Location = System::Drawing::Point(59, 203);
+			this->button1->Margin = System::Windows::Forms::Padding(4);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(100, 28);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Evaluate";
 			this->button1->UseVisualStyleBackColor = true;
@@ -67,29 +70,48 @@ namespace Calculator_Project {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(31, 39);
+			this->textBox1->Location = System::Drawing::Point(41, 48);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(375, 48);
+			this->textBox1->Size = System::Drawing::Size(499, 58);
 			this->textBox1->TabIndex = 1;
 			this->textBox1->Text = L"abc";
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(285, 203);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(96, 28);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Clear";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(440, 307);
+			this->ClientSize = System::Drawing::Size(587, 378);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm";
 			this->Text = L"Main Window";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
+
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 		textBox1->Text = "hellow";
 	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	
+			 textBox1->Text = "";
+	}
+
 	};
 }
